9592c76204fc926b962f769011c4e8dcf8139be8
Vasya Ponomarenko <777Vasya77@gmail.com>
2019-02-27 10:35:14 +0200

    Заменил for...of на map

--- src/get-repeat-days-markdown.js
+++ src/get-repeat-days-markdown.js
@@ -1,19 +1,12 @@
 export default (days) => {
-  const daysMarkdown = [];
-  for (const [key, value] of Object.entries(days)) {
-    const dayMarkdown = `
-        <input 
-            class="visually-hidden card__repeat-day-input" 
-            type="checkbox" 
-            id="repeat-${key}-6" 
-            name="repeat" 
-            value="${key}" 
-            ${value ? `checked` : ``}>
-        <label class="card__repeat-day" for="repeat-${key}-6">${key}</label>
-    `;
-
-    daysMarkdown.push(dayMarkdown);
-  }
-
-  return daysMarkdown.join(``);
+  return Object.entries(days).map(([key, value]) => {
+    return `<input 
+                class="visually-hidden card__repeat-day-input" 
+                type="checkbox" 
+                id="repeat-${key}-6" 
+                name="repeat" 
+                value="${key}" 
+                ${value ? `checked` : ``}>
+            <label class="card__repeat-day" for="repeat-${key}-6">${key}</label>`;
+  }).join(``);
 };
